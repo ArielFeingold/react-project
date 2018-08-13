a30db1eb703f275f43384f087ab2fbfecba6c6e1
ArielFeingold <feingold.ariel@gmail.com>
2018-08-13 13:21:07 -0400

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   resources :exercises
   resources :workouts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/auth/facebook/callback' => 'sessions#create'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
 end
