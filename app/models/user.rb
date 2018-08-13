27dd54ae2d0f00501516da3b342a01bda08c6ade
ArielFeingold <feingold.ariel@gmail.com>
2018-08-13 11:52:16 -0400

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   has_many :workouts
   has_many :exercises
   has_many :stats
-  
+
+  validates :email, :password, presence: true, on: :create
+  validates :name, presence: true
+  validates :password, confirmation: true
+  validates :email, uniqueness: true
+  validates :about, presence: true, on: :update
+
 end
